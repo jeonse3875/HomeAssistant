800e9164515721d39f6bd6fb5505b85a878d7083
jeonse3875 <devbyseke@gmail.com>
2021-12-03 04:10:47 +0900

    Fix

--- CampusManager.py
+++ CampusManager.py
@@ -29,7 +29,7 @@ def GetAssignmentList(day):
     options.add_argument("disable-gpu")
     options.add_argument("lang=ko_KR")
     options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    driver = webdriver.Chrome(chrome_options=options)
+    driver = webdriver.Chrome('./chromedriver',chrome_options=options)
 
     driver.implicitly_wait(1)
 
